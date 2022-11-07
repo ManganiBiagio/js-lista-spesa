0367d1b3735e96b30ae0a14406a0cb69c11ad0d3
ManganiBiagio <biagiomangani@live.it>
2022-11-07 14:30:36 +0100

    lista spesa toogle

--- js/spesa.js
+++ js/spesa.js
@@ -1,11 +1,19 @@
 const listaSpesa=["pane","latte","pomodori","carote","salmone"];
 const outListaEl=document.querySelector(".output-lista");
-console.log(outListaEl)
 
+const listElements=[];
 let i=0;
 while(i<listaSpesa.length){
-    console.log(listaSpesa[i]);
+    const liEl=document.createElement("li");
+    liEl.append(`${listaSpesa[i]}`);
+    outListaEl.append(liEl)
+    listElements.push(liEl);
+    // outListaEl.innerHTML+=`<li>${listaSpesa[i]}</li>`;
+    liEl.addEventListener("click",function(){
+        liEl.classList.toggle("checked");
 
-    outListaEl.innerHTML+=`<li>${listaSpesa[i]}</li>`;
+    })
+
     i++;
 }
+
