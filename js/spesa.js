a93684cc6cb8bbbd83e33c4c0295d3f43b8968fc
ManganiBiagio <biagiomangani@live.it>
2022-11-07 14:15:56 +0100

    html

--- js/spesa.js
+++ js/spesa.js
@@ -1,7 +1,11 @@
 const listaSpesa=["pane","latte","pomodori","carote","salmone"];
+const outListaEl=document.querySelector(".output-lista");
+console.log(outListaEl)
 
 let i=0;
 while(i<listaSpesa.length){
     console.log(listaSpesa[i]);
+
+    outListaEl.innerHTML+=`<li>${listaSpesa[i]}</li>`;
     i++;
 }
